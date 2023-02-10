08bc7bcd87e6ed21c1de3ddbea9d28490c0685da
Adolfo Cortez <jose.a.cortez.cerda@accenture.com>
2023-02-10 11:11:17 -0600

    Added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //first change
+        //my Second change
         [Key]
         public int CartItemId { get; set; }
 
